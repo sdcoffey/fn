3e067871db0cfc9711f26faf84c7e33527ca17df
sdcoffey <stevendcoffey@gmail.com>
2023-12-04 12:42:23 -0800

    fixup! add publish script

--- publish.sh
+++ publish.sh
@@ -14,7 +14,14 @@ if [[ $new_tag =~ ^v[0-9]+\.[0-9]+ ]]; then
   git push origin $new_tag
   git push origin main
 
+  echo "Updating go index"
   GOPROXY=proxy.golang.org go list -m "github.com/sdcoffey/fn@$new_tag"
+
+  echo "Creating release"
+  tmpfile=$(mktemp)
+  vim $tmpfile
+
+  gh release create $new_tag -F $tmpfile -t $new_tag --latest
 else
   echo "Invalid tag: $new_tag -- must be in the form v0.0.0"
   exit 1
